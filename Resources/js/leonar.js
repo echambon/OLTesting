9edac10356f2f91060007be6ef99ad4f6ce61eac
Emmanuel Chambon <contact@emmanuel-chambon.fr>
2020-08-04 14:59:02 +0200

    cleanup

--- Resources/js/leonar.js
+++ Resources/js/leonar.js
@@ -343,7 +343,6 @@ function init() {
 
     map.on('click', function (evt) {
         var coordinate = map.getEventCoordinate(evt.originalEvent);
-
         point = interactWithVectorSource(debugGPXSource, coordinate); // TODO : loop over all sources in workspace
 
         // Force map update (otherwise it is updated on next loop() call)
